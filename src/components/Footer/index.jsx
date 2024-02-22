899397627cb048524199d584e15fb581af03c9fd
Gildásio Aguiar <gildasioirr@gmail.com>
2024-02-22 14:53:24 -0300

    feat: some features added

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+
+export function Footer() {
+    return (
+      <footer className={styles.footer}>
+        Feito por Gildásio Aguiar &copy; 2024
+      </footer>  
+    )
+}
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,15 +1,32 @@
-import styles from './styles.module.css'
+import { useState } from 'react'
+import styles from './Header.module.css'
 
 export function Header() {
+
+    const [ showMenu, setShowMenu ] = useState(false)
+    const toogleMenu = () => {
+        setShowMenu(!showMenu)
+    }
+
     return (
         <header className={styles.header}>
             <span>GilAguiar.dev</span>
-            <nav>
+            <nav 
+                className={`${styles.menuSandwich} ${ showMenu ? styles.show : '' }`}
+                onClick={toogleMenu}
+            >
                 <a href="#">Home</a>
                 <a href="#">Sobre</a>
                 <a href="#">Projetos</a>
                 <a href="#">Contatos</a>
             </nav>
+            <div className={styles.menuButton}
+                 onClick={toogleMenu}
+            >
+                <span className={styles.linha}> </span>
+                <span className={styles.linha}> </span>
+                <span className={styles.linha}> </span>
+            </div>
         </header>
     )
 }
